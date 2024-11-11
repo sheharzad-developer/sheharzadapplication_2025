950652c4af3f8ba5424196c52856d9452a03aecf
Sheharzad <sheharzad.salahuddin9000@outlook.com>
2024-11-12 01:16:56 +0500

    Assets & Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,13 @@ const spaceGrotesk = localFont({
 
 export const metadata: Metadata = {
   title: "Sheharzad Application",
-  description: "A better version of Sheharzad application",
+
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 const Home = () => {
   <>
     <h1 className="h1-bold">Welcome to NextJS</h1>;
-    <h1 className="h2-bold font-inter">Welcome to NextJS</h1>;
-    <h1 className="h3-bold">Welcome to NextJS</h1>;
   </>;
 };
 
