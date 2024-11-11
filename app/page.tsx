44dd3be0e8352b16cefef64845973c47d1decaf0
Sheharzad <sheharzad.salahuddin9000@outlook.com>
2024-11-12 00:02:04 +0500

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="text-3xl text-violent-700 font-black">Welcome</h1>;
+  return <h1 className="text-3xl text-violent-700 font-black">Welcome to NextJS</h1>;
 }
