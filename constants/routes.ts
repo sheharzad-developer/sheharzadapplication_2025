c9266055e8d496e663be06d07c9ae395becb1b1c
Sheharzad <sheharzad.salahuddin9000@outlook.com>
2024-11-12 18:16:44 +0500

    Setup Auth Layouts, Social Auth Form, Next Auth, OAuth with Google & Github

---
+++ constants/routes.ts
@@ -0,0 +1,12 @@
+// import { redirect } from "next/dist/server/api-utils";
+
+const ROUTES = {
+  HOME: "/",
+  SIGN_IN: "/sign-in",
+  SIGN_UP: "/sign-up",
+};
+
+export default ROUTES;
+
+// redireect("/sign-in-something");
+// redireect(ROUTES.SIGN_IN);
